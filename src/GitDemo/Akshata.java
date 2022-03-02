cd43aa266cd207cd4ec670d8c394f3dd98a02974
akshata-1207 <akshatadeshpande0047@gmail.com>
2022-03-02 10:51:35 +0530

    Akshata test 2

---
+++ src/GitDemo/Akshata.java
@@ -0,0 +1,19 @@
+package GitDemo;
+
+public class Akshata {
+
+	public static void main(String[] args) {
+		
+				String hungry="yes";
+				if(hungry=="yes")
+				{
+					System.out.println("eat something..");
+				}
+				else
+				{		
+					System.out.println("Do work..");
+				}
+
+			}
+
+		}
